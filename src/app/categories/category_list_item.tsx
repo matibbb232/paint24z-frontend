166f3538c6cde5feaf1936c8ce269df656a774a0
Uziemblo_Julian <01169369@pw.edu.pl>
2025-01-26 10:44:22 +0100

    Fix integration with backend categories

--- src/app/categories/category_list_item.tsx
+++ src/app/categories/category_list_item.tsx
@@ -11,7 +11,7 @@ type CategoryListItemProps = {
 export default function CategoryListItem(props: CategoryListItemProps) {
     const router = useRouter();
     return (
-        <div onClick={() => router.push(`/?category=${props.name}`)} className="flex flex-1 flex-col gap-5 justify-center items-center rounded-3xl bg-white pt-5 pb-12 hover:cursor-pointer ease-in-out duration-300 hover:bg-neutral-200">
+        <div onClick={() => router.push(`/?category=${props.id}`)} className="flex flex-1 flex-col gap-5 justify-center items-center rounded-3xl bg-white pt-5 pb-12 hover:cursor-pointer ease-in-out duration-300 hover:bg-neutral-200">
             <span className="text-black font-jaro text-center text-7xl">{props.name}</span>
             <img src={props.img_url} className="w-[400px] aspect-square rounded-xl"/>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,7 +72,7 @@ function Index() {
     };
 
     fetchDataForPosts();
-  }, []);
+  }, [searchParams]);
 
   return (
     <>
